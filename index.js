cf32c245b396b9438338e153676c2f686663c71c
Purefallen11 <53922216+Purefallen11@users.noreply.github.com>
2022-02-16 19:57:54 -0600

    idk how to spell, fixed the bug

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const dotenv = require('dotenv').config();
-
+const cors = require('cors')
 const express = require('express');
 const app = express()
 const port = process.env.PORT || 9000
 
-app.use(cores());
+app.use(cors());
 app.use(express.json())
 
 app.use('/api/*', (req, res) => {
